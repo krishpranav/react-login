cf8348fd0c77248ad89d8d2d41423c57dcb1a531
elangosimsol <elangovan@simsol.in>
2022-11-14 19:31:55 +0530

    react-ui

---
+++ react-login/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>Made by @krishpranav</footer>
+    )
+  }
+}
+
+export default Footer;
